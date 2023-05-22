e234b73759b8484aae3c2ad13d36784837f8b8eb
Uglykidmat <mathieu.peyrot.47@gmail.com>
2023-05-22 14:12:13 +0200

    Update Home Faker
    Update Home Faker

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-
+use Faker\Factory;
 use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(ArticleRepository $repo)
     {
-
+        $faker = Factory::create("fr_FR");
+        $title = $faker->text();
+        dump($title);
         return $this->render('home/index.html.twig', [
             "articles" => $repo->findLastArticles(3)
         ]);
